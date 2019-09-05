4814890485c4aae856272559a3a19c836ee3d631
namelessking <habibur.tanjin3@gmail.com>
2019-09-05 15:20:33 +0200

    added unite test for FullName propertie of Customer objects

---
+++ Acme.BL.Tests/CustomerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.BL.Tests
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // -- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Gino",
+                LastName = "Panino"
+            };
+
+            string expected = "Gino, Panino";
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
