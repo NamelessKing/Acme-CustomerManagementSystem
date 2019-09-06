72885668e2f6769bbdbee9ae217874525e83c8bd
namelessking <habibur.tanjin3@gmail.com>
2019-09-06 23:35:41 +0200

    finished structuring the Customer class, implementation of Retrieve and Save methods are missing

--- Acme.BL/Customer.cs
+++ Acme.BL/Customer.cs
@@ -8,6 +8,16 @@ namespace Acme.BL
 {
     public class Customer
     {
+        public Customer()
+        {
+
+        }
+
+        public Customer(int customerId)
+        {
+            CustomerId = customerId;
+        }
+
         public int CustomerId { get; private set; }
         public string EmailAddess { get; set; }
         public string FirstName { get; set; }
@@ -32,6 +42,48 @@ namespace Acme.BL
 
         public static int InstanceCount { get; set; }
 
+        /// <summary>
+        /// Retrieve one cutomer by id.
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Retrieve all customers.
+        /// </summary>
+        /// <returns></returns>
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        /// <summary>
+        /// Save the current customer.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Validates the customer data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddess)) isValid = false;
+
+            return isValid;
+        }
 
     }
 }
