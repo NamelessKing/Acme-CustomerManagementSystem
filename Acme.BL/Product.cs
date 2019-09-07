4dbfb134b18949690773800f18d5753229e16126
namelessking <habibur.tanjin3@gmail.com>
2019-09-07 21:56:13 +0200

    added Order, OrderItem and Product classes

---
+++ Acme.BL/Product.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.BL
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductId { get; private set; }
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Retrieve one product by id.
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public Product Retrieve(int productId)
+        {
+            return new Product();
+        }
+
+        /// <summary>
+        /// Retrieve all products.
+        /// </summary>
+        /// <returns></returns>
+        public List<Product> Retrieve()
+        {
+            return new List<Product>();
+        }
+
+        /// <summary>
+        /// Save the current product.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Validates the product data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
+
+    }
+}
