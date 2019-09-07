a1787fe3ef9b6a4de33c11f09e313978ddaf85b7
namelessking <habibur.tanjin3@gmail.com>
2019-09-07 23:47:42 +0200

    added CutomerRepository

---
+++ Acme.BL/Repository/CustomerRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.BL.Repository
+{
+    public class CustomerRepository
+    {
+
+        private List<Customer> FakeCustomersData = new List<Customer>()
+        {
+            new Customer(1)
+            {
+                EmailAddess = "gino@email.com",
+                FirstName = "Gino",
+                LastName = "Panino"
+            },
+            new Customer(2)
+            {
+                EmailAddess = "pino@email.com",
+                FirstName = "Pino",
+                LastName = "Merlino"
+            },
+            new Customer(3)
+            {
+                EmailAddess = "Brio@email.com",
+                FirstName = "Brio",
+                LastName = "Blue"
+            }
+        };
+
+        /// <summary>
+        /// Retrieve one cutomer by id.
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer Retrieve(int customerId)
+        {
+            Customer customer = FakeCustomersData.Find(c => c.CustomerId == customerId);
+
+            return customer;
+        }
+
+
+        /// <summary>
+        /// Retrieve all customers.
+        /// </summary>
+        /// <returns></returns>
+        public List<Customer> Retrieve()
+        {
+            return FakeCustomersData;
+        }
+
+
+        /// <summary>
+        /// Save the current customer.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
